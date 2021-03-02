90c7d491fa12edb6aaab539078310fa45238fa9e
massiliatazit <massiliatazit@users.noreply.github.com>
2021-03-02 15:53:54 +0100

    rooms and stuffs

--- src/socket.js
+++ src/socket.js
@@ -1,10 +1,39 @@
 const socketio = require('socket.io')
+const {addUserToRoom,getUsersInRoom} = require("./utils/users")
 const createSocketServer = server => {// create to server
     const io = socketio(server)
-    io.on("connection",socket =>{ // connect to server
+    io.on("connection", socket =>{ // connect to server
         console.log(`New socket connection --> ${socket.id}`)
-        socket.on("message",()=>{}) // listen to messages to handle them.
-        
+        socket.on("joinRoom", async data => {
+            try {
+              // add user to specified room (in mongo)
+              console.log({...data})
+              const { username, room } = await addUserToRoom({
+                socketId: socket.id,
+                ...data,
+                
+              })
+      
+              socket.join(room)
+      
+              const messageToRoomMembers = {
+                sender: "Admin",
+                text: `${username} has joined the room!`,
+                createdAt: new Date(),
+              }
+      
+              socket.broadcast.to(room).emit("message", messageToRoomMembers) // sending the message to all the users connected in the room
+      
+              // send rooms info (users list) to all users (example: when someone joins/leaves room)
+              const roomMembers = await getUsersInRoom(room)
+      
+              io.to(room).emit("roomData", { room, users: roomMembers }) // server sending server message to all members
+            } catch (error) {
+              console.log(error)
+            }
+          }) // joining chat room// client join the room
+        socket.on("sendmessage",({room,message})=>{}) // client send a message.
+        socket.on("leaveRoom",()=>{}) // client leaves the room
 
     }) 
 
---
+++ src/utils/users.js
@@ -0,0 +1,69 @@
+const RoomModel = require("../services/rooms/schema");
+const addUserToRoom = async ({ sender, receiver, socketId, room }) => {
+  try {
+    const roomName = room ? room : await createRoom(sender, receiver);
+    const user = await RoomModel.findOne({
+      name: roomName,
+      "members.username": sender,
+    });
+    const user1 = await RoomModel.findOne({
+      name: roomName,
+      "members.username": receiver,
+    });
+    if (user) {
+      // if user is in room update socketId
+      await RoomModel.findOneAndUpdate(
+        { name: roomName, "members.username": sender },
+        { "members.$.socketId": socketId } // replace prev Id with the new socketId
+      );
+    } else {
+      // if doesn't exist add it to members
+      await RoomModel.findOneAndUpdate(
+        { name: roomName },
+        { $addToSet: { members: { username: sender, socketId } } }
+      );
+    }
+    if (user1) {
+      // if user is in room update socketId
+      await RoomModel.findOneAndUpdate(
+        { name: roomName, "members.username": receiver },
+        { "members.$.socketId": socketId } // replace prev Id with the new socketId
+      );
+    } else {
+      // if doesn't exist add it to members
+       await RoomModel.findOneAndUpdate(
+        { name: roomName },
+        { $addToSet: { members: { username: receiver, socketId } } }
+      );
+    }
+    return { receiver, roomName };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// on join if room exist ? joın : create
+const createRoom = async (sender, receiver) => {
+  try {
+    const newRoom = await new RoomModel({ name: sender + receiver });
+
+    const savedRoom = await newRoom.save();
+    if (savedRoom._id) return savedRoom.name;
+  } catch (error) {
+    console.log(error);
+  }
+};
+const getUsersInRoom = async (roomName) => {
+  try {
+    const room = await RoomModel.findOne({ name: roomName });
+
+    return room.members;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  addUserToRoom,
+  getUsersInRoom,
+};
